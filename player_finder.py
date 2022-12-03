2a610bb13f29ba11f4bda38130ccb7e39c1e9c21
Pranay Mundra <pranaymundra4510@gmail.com>
2022-12-03 15:56:10 -0500

    player finder done

--- player_finder.py
+++ player_finder.py
@@ -14,9 +14,13 @@ DATA_LOC = './data/{0}_data_fb_ref.csv'
 2. normalize the numeric columns using : z-score normalization
 3. build the data matrix and idMap
 '''
-def load_and_preprocess_data(params):
-    data = pd.read_csv(DATA_LOC.format(params.league))
-    data_season_mask = data['Season'] == params.season
+def load_and_preprocess_data(params, query=False):
+    if query:
+        data = pd.read_csv(DATA_LOC.format(params.query_league))
+        data_season_mask = data['Season'] == params.query_season
+    else:
+        data = pd.read_csv(DATA_LOC.format(params.league))
+        data_season_mask = data['Season'] == params.season
     data = data[data_season_mask]
     data_numeric_cols = data.select_dtypes(include='number')
     data_numeric_cols_normalized = (data_numeric_cols - data_numeric_cols.mean()) / (data_numeric_cols.std())
@@ -49,37 +53,52 @@ def build_index(data):
     d = 182
     data[:, 0] += np.arange(N) / 1000.
     index = faiss.IndexFlatL2(d)
-    print(index.is_trained)
+    # print(index.is_trained)
     index.add(data)
-    print(index.ntotal)
+    # print(index.ntotal)
     return index 
 
 def player_finder(index, idMap, data, params):
-    params.league = 'bundesliga'
-    query_data, query_idMap = load_and_preprocess_data(params)
-    player_name = 'alphonso davies'
-    idx = query_idMap.index(player_name)
-    print(idx)
-    # D, I = index.search(query_data[:5], params.k)
-    query_kdb = np.zeros((1, 182))
-    query_kdb[0] = query_data[idx]
-    query_kdb = query_kdb.astype('float32')
-    D, I = index.search(query_kdb, params.k)
-    print(I)
-    first_search = query_idMap[:5]
-    for idx, nbs in enumerate(I):
-        similar_players = [idMap[n] for n in nbs]
-        print('Query player : {0}\t Similar Players : {1}'.format(player_name, ','.join(similar_players)))
-    # print(D)
+    query_data, query_idMap = load_and_preprocess_data(params, query=True)
+    print('Index Built on {0} for Season:{1} || Query using {2} for Season:{3}\n'.format(params.league, params.season, params.query_league, params.query_season))
+    if params.player_name:
+        try:
+            idx = query_idMap.index(params.player_name)
+        except ValueError:
+            print('player name not in the same league, season')
+            exit()
+        query_player = np.zeros((1, 182))
+        query_player[0] = query_data[idx]
+        query_player = query_player.astype('float32')
+        D, I = index.search(query_player, params.k)
+        for idx, nbs in enumerate(I):
+            similar_players = [idMap[n] for n in nbs]
+            print('Query player : {0}'.format(params.player_name))
+            for idx_2, s in enumerate(similar_players):
+                print('{0} : {1}'.format(s, D[idx][idx_2]))
+            print('**********')
+    else:        
+        D, I = index.search(query_data, params.k)
+        for idx, nbs in enumerate(I):
+            similar_players = [idMap[n] for n in nbs]
+            print('Query player : {0}'.format(query_idMap[idx]))
+            for idx_2, s in enumerate(similar_players):
+                print('{0} : {1}'.format(s, D[idx][idx_2]))
+            print('**********')
+        # print(D)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--league', type=str, default='premier_league', help='name of league to build index over = [premier_league, la_liga, bundesliga, ligue_1, serie_a]')
     parser.add_argument('--season', type=str, default='2020-2021', help='season for which we are searing = [2017-2018, 2018-2019, 2019-2020, 2020-2021, 2021-2022]')
+    parser.add_argument('--query_league', type=str, default='bundesliga', help='name of the league to use as query')
+    parser.add_argument('--query_season', type=str, default='2020-2021', help='season to use as query')
+    parser.add_argument('--player_name', type=str, default=None, help='name of the player (from query league) [firstname_lastname] to find similar players from, if not given we go over each player of the query league')
     parser.add_argument('--k', type=int, default=10, help='k for the top-k search')
     params = parser.parse_args()
-
+    if params.player_name:
+        params.player_name = ' '.join(params.player_name.split('_'))
     data, idMap = load_and_preprocess_data(params)
     index = build_index(data)
     player_finder(index, idMap, data, params)
